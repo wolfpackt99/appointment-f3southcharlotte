e66ab6d22312615dbecb1a7e2792bd2c278305c5
Jones, Trenton D. (LNG-RDU) <trent.jones@lexisnexis.com>
2017-07-18 08:45:25 -0400

    add comment count to tag page

--- tag.php
+++ tag.php
@@ -54,6 +54,23 @@
 												<?php echo get_the_date('M j, Y'); ?></a>
 											</div>
 											<p><?php echo get_home_blog_excerpt(); ?></p>
+											<?
+											$num_comments = get_comments_number(); // get_comments_number returns only a numeric value
+
+											if ( comments_open() ) {
+												if ( $num_comments == 0 ) {
+													$comments = __('No Comments');
+												} elseif ( $num_comments > 1 ) {
+													$comments = $num_comments . __(' Comments');
+												} else {
+													$comments = __('1 Comment');
+												}
+												$write_comments = '<a href="' . get_comments_link() .'">'. $comments.'</a>';
+											} else {
+												$write_comments =  __('Comments are off for this post.');
+											} 
+											echo $write_comments;
+											?>
 										</div>
 									</div>
 							</div>
