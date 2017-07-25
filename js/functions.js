66c5796e55d2f88eeb2408a88d21e89b1829c228
Jones, Trenton D. (LNG-RDU) <trent.jones@lexisnexis.com>
2017-07-25 13:38:40 -0400

    update tclaps functions

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,29 @@
-jQuery(document).ready(function(){
-    
-    var tclapsEnablement = function(){
+jQuery(document).ready(function (Cookies) {
+    'use strict';
+    var tclapsEnablement = function () {
         var btn = jQuery("#btn-tclap");
         var post_id = btn.data("post_id");
         var tclapped = Cookies.getJSON('tclaps');
-        if (!tclapped) tclapped = {};
+        var date = {};
 
-        function init(){
-            if(btn.length >0){
-                if (tclapped[post_id])
-                {
-                    btn.attr("disabled","disabled");
-                }
-                else{
-                    btn.click(enableClick);
-                }
-            }
-        }
-
-        function enableClick(e)
-        {
+        function enableClick(e) {
             var $this = jQuery(this);
             e.preventDefault();
             var thumbs = $this.find(".fa.fa-thumbs-up");
             var spinner = $this.find(".fa.fa-spinner");
-            if (!tclapped[post_id]){
-                $this.attr("disabled","disabled");
+
+            function showSpinner(show) {
+                if (show) {
+                    thumbs.hide();
+                    spinner.show();
+                } else {
+                    thumbs.show();
+                    spinner.hide();
+                }
+            }
+
+            if (!tclapped[post_id]) {
+                $this.attr("disabled", "disabled");
                 showSpinner(true);
                 jQuery.ajax({
                     type: "post",
@@ -35,37 +33,44 @@ jQuery(document).ready(function(){
                         action: "my_user_tclap",
                         post_id: post_id
                     }
-                }).done(function(response){
-                    if (response.data.type ==='success'){
+                }).done(function (response) {
+                    if (response.data.type === 'success') {
                         $this.find('.tclap-counter').html(response.data.tclaps);
-                        $this.attr("disabled","disabled");
+                        $this.attr("disabled", "disabled");
                         tclapped[post_id] = true;
-                        Cookies.set('tclaps', tclapped, { expires: 14 });
-                    }
-                    else{
+                        Cookies.set('tclaps', tclapped, {
+                            expires: new Date(tclapped.thedate)
+                        });
+                    } else {
                         $this.removeAttr("disabled");
-                        console.log('tclap error on server.');
                     }
-
-                }).always(function(){
+                }).always(function () {
                     showSpinner(false);
                 });
             }
+        }
 
-            function showSpinner(show)
-            {
-                if (show){
-                    thumbs.hide()
-                    spinner.show();
-                }
-                else{
-                    thumbs.show();
-                    spinner.hide();
+        function init() {
+            date = new Date();
+            date.setDate(date.getDate() + 7);
+            if (!tclapped) {
+                tclapped = {};
+                tclapped.thedate = date;
+            }
+            if (!tclapped.thedate) {
+                tclapped.thedate = date;
+            }
+            if (btn.length > 0) {
+                if (tclapped[post_id]) {
+                    btn.attr("disabled", "disabled");
+                } else {
+                    btn.click(enableClick);
                 }
             }
-        }      
-        init(); 
+        }
+
+        init();
     };
 
     tclapsEnablement();
-});
+}(Cookies));
